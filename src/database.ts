260f6298a4a5415dfb327957265db4c0c29442ad
Guilherme <guihsn2007@hotmail.com>
2023-10-25 09:32:21 -0300

    Initial commit

---
+++ src/database.ts
@@ -0,0 +1,19 @@
+import { Knex, knex as setupKnex } from 'knex'
+import { env } from './env'
+
+if (!process.env.DATABASE_URL) {
+  throw Error('DATABASE_URL not found')
+}
+export const config: Knex.Config = {
+  client: 'sqlite',
+  connection: {
+    filename: env.DATABASE_URL,
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = setupKnex(config)
